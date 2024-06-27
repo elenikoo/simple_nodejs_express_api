71689afcabacdc540ce4b55c3b5048a82ac07f5c
elenikoo <22100505@ibsu.edu.ge>
2024-06-27 22:34:35 +0400

    commit

--- project/app.js
+++ project/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const studentsRouter = require('./routes/students');
+const productRouter = require('./routes/products');
 
 var app = express();
 
@@ -23,7 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/students', studentsRouter);
-app.use('/files', filesRouter);
+app.use('/students', productRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
