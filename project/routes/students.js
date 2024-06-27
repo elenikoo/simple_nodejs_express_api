d745351ce22d08d11d4de5e8a666d176f7583784
elenikoo <22100505@ibsu.edu.ge>
2024-06-27 14:36:22 +0400

    commit

---
+++ project/routes/students.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const studentService = require('../services/studentService');
+
+
+router.get('/all', studentService.getAll);
+router.get('/:id', studentService.getOne);
+router.post('/add',  studentService.add);
+router.delete('/:id',  studentService.delete);
+router.put('/:id', studentService.update);
+
+module.exports = router;
